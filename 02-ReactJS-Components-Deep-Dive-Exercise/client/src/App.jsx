9ef530b147b7152f15415d95b4f45061aaf3f9c8
Slavi Ovardov <ovardovslav@gmail.com>
2025-11-22 17:21:24 +0200

    add base layout components

--- 02-ReactJS-Components-Deep-Dive-Exercise/client/src/App.jsx
+++ 02-ReactJS-Components-Deep-Dive-Exercise/client/src/App.jsx
@@ -1,7 +1,25 @@
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
+import Search from "./components/Search.jsx";
+import UserList from "./components/UserList.jsx";
+import Pagination from "./components/Pagination.jsx";
+
 function App() {
    return (
       <div>
-         <h1>Test</h1>
+         <Header />
+
+         <main className="main">
+            <section className="card users-container">
+               <Search />
+
+               <UserList />
+
+               <Pagination />
+            </section>
+         </main>
+
+         <Footer />
       </div>
    );
 }
