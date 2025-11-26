6e64b906a56d4e6c7c4d97b1b33e818f327a59c3
Slavi Ovardov <ovardovslav@gmail.com>
2025-11-26 22:18:51 +0200

    renaming component folder

--- 03-ReactJS-Routing-Exercise/src/components/catalog/Catalog.jsx
+++ 03-ReactJS-Routing-Exercise/src/components/catalog/Catalog.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import GameCard from "../game/GameCard";
+import GameCard from "../game-card/GameCard";
 
 const API_URL = "http://localhost:3030/jsonstore/games";
 
