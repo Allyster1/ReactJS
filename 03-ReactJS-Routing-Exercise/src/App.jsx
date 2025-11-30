a7cc3c21f821932edc85a3c8e96331344eb204d5
Slavi Ovardov <ovardovslav@gmail.com>
2025-11-30 21:22:50 +0200

    add register and validation

--- 03-ReactJS-Routing-Exercise/src/App.jsx
+++ 03-ReactJS-Routing-Exercise/src/App.jsx
@@ -6,6 +6,7 @@ import Footer from "./components/footer/Footer";
 import Catalog from "./components/catalog/Catalog";
 import Details from "./components/details/Details";
 import GameCreate from "./components/game-create/GameCreate";
+import Register from "./components/register/Register";
 
 function App() {
    return (
@@ -17,6 +18,7 @@ function App() {
             <Route path="/catalog" element={<Catalog />} />
             <Route path="games/:gameId/details" element={<Details />} />
             <Route path="games/create" element={<GameCreate />} />
+            <Route path="/register" element={<Register />} />
          </Routes>
 
          <Footer />
