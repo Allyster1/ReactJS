198fbcbe4451c87f082f4089c48ee4c19f33a125
Slavi Ovardov <ovardovslav@gmail.com>
2025-12-05 19:56:52 +0200

    lift user state to App component

--- 03-ReactJS-Routing-Exercise/src/components/register/Register.jsx
+++ 03-ReactJS-Routing-Exercise/src/components/register/Register.jsx
@@ -1,10 +1,8 @@
-export default function Register() {
+export default function Register({ user, onRegister }) {
    const registerSubmit = (formData) => {
       const email = formData.get("email");
       const password = formData.get("password");
       const confirmPassword = formData.get("confirm-password");
-
-      // TODO: Validation
 
       if (!email || !password) {
          return alert("Email and password are required!");
@@ -14,7 +12,8 @@ export default function Register() {
          return alert("Password does not match!");
       }
 
-      // TODO: Register User
+      // Register User
+      onRegister(email);
 
       // TODO: Redirect to home page
    };
@@ -25,6 +24,7 @@ export default function Register() {
             <div className="container">
                <div className="brand-logo"></div>
                <h1>Register</h1>
+               {user && <h2>You are already registered with {user.email}</h2>}
 
                <label htmlFor="email">Email:</label>
                <input type="email" id="email" name="email" placeholder="Your Email" />
