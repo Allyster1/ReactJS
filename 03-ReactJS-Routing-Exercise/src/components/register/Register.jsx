d72bbb7c34d51b8410dda6e593cbab62d11a4439
Slavi Ovardov <ovardovslav@gmail.com>
2025-12-05 20:37:22 +0200

    adding login functionality

--- 03-ReactJS-Routing-Exercise/src/components/register/Register.jsx
+++ 03-ReactJS-Routing-Exercise/src/components/register/Register.jsx
@@ -16,11 +16,15 @@ export default function Register({ onRegister }) {
          return alert("Password does not match!");
       }
 
-      // Register User
-      onRegister(email);
+      try {
+         // Register User
+         onRegister(email, password);
 
-      // Redirect to home page
-      navigate("/");
+         // Redirect to home page
+         navigate("/");
+      } catch (err) {
+         alert(err.message);
+      }
    };
 
    return (
